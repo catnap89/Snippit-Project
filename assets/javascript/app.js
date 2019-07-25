d3054f1c940d26a6aaa253382a81377e83a16944
youngwoo.cho <petshop0907@gmail.com>
2019-07-25 15:37:42 -0400

    removed codemirror lib and using cdn instead

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,41 +13,30 @@ $(document).ready(function() {
     // On click Save Snippit button (#modal-save-btn), push the snippit into firebase database and dynamically generate card with the code snippit.
     $('#modal-save-btn').on('click', saveSnippit);
 
-    // codemirror
-
-    var code = $('.codemirror-textarea')[0];
-    // Declaring editor variable and settings (lineNubers, lineWrapping..etc) to pass from CodeMirror.fromTextArea function code variable.
-    var editor = CodeMirror.fromTextArea(code, {
-   
-        lineNumbers: true,
-        lineWrapping : true,
-        
-
-    })
 
     // saveSnippit function
     function saveSnippit() {
         // Declare variables
         var editKey = ''; // variable to store reference key to firebase for editing correspond data
-        var codeMirrorVal = $('#codeMirror-input').val().trim(); // grab input value
-        var snippit; // maybe this can be used somehow
+        var textarea = $('.Codemirror-Code');
+        var snippit = editor.getValue("\n"); // grab value of the codemirror textarea 
         var userId; // maybe this can be used somehow
 
-        // Using conditional statement to prevent data to be pushed to the database when not every form is filled
-        if (codeMirrorVal != '') {
+        // Using conditional statement to prevent data to be pushed to the database when there is no codes writeen in the textaera.
+        if (textarea != '') {
             // Clear CodeMirror input field (Not sure if this is needed since we haven't used codemirror yet. When new modal is opened, is the input field going to be cleared automatically?)
-            $('#codeMirror-input').val('');
+            // $(editor.).val('');
 
             // Push to firebase
             if (editKey == '') { // If there is no editKey value
             // NEED TO KNOW THE DATABASE STRUCTURE WE WILL USE. LIKE IF WE WILL MAKE CHILD FOR EACH USER?
                 database.ref().child('snippits').push({ // push the variables to the database in a child named 'snippits'
-                snippit: codeMirrorVal,
+                snippit: snippit,
                 userId: userId,
                 })
             } else if (editKey !== '') { // If there is editKey value --- THIS WILL BE USED FOR UPDATING EXISTING DATA. IF WE LET USERS TO CHANGE, MODIFY, ETC PREVIOUSLY SAVED SNIPPIT
                 database.ref('snippits/' + editKey).update({ // Locate database with provided editKey value as it's unique key in 'snippits' path and update database
-                snippit: codeMirrorVal,
+                snippit: snippit,
                 userId: userId,       
                 })
                 editKey = ''; // Empty the editKey value once else if conditional is met and database is updated
@@ -59,42 +48,3 @@ $(document).ready(function() {
 
 })
 
-
-
-/*
-<!-- Button trigger modal-->
-<button type="button" class="btn btn-outline-success my-2 my-sm-0" data-toggle="modal" data-target="#main-modal-div" id="main-add-btn">Create Snippit</button>
-//
-<div class="modal" tabindex="-1" role="dialog" id="main-modal-div">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Snippit</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Cancel">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <p>Modal body text goes here.</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal"
-                    id="model-cancel-btn">Cancel</button>
-                <button type="button" class="btn btn-primary" id="modal-save-btn">Save</button>
-            </div>
-        </div>
-    </div>
-</div>
-==============
-
-<div class="card bg-light mb-3" style="width: 50rem;">
-    <div class="card-header">Header</div>
-    <div class="card-body">
-        <h5 class="card-title">Light card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-            card's content.</p>
-    </div>
-</div>
-
-
-*/
--- assets/javascript/codemirror.js
+++ assets/javascript/codemirror.js
@@ -1,1 +1,112 @@
 // this will be used for codemirror
+
+// codemirror
+
+// var code = $('.code-editor-textarea')[0];
+// Declaring editor variable and settings (lineNubers, lineWrapping..etc) to pass from CodeMirror.fromTextArea function code variable.
+// var editor = CodeMirror.fromTextArea(code, {
+//     lineNumbers: true,
+//     lineWrapping : true,
+//     cursorBlinkRate: 0,
+    
+// })
+
+var htmlEditor = CodeMirror.fromTextArea(document.getElementById("codemirror"), {
+	lineNumbers: true,			// gives a lineNumber gutter
+	mode: 'htmlmixed',			// sets syntax mode
+	theme: 'material',			// select theme
+	
+	indentUnit: 4,				// default is 2
+	tabSize: 4,					// default already is 4
+	indentWithTabs: true,		// default is false
+	
+	fixedGutter: true,
+	coverGutterNextToScrollbar: true,
+	
+	showCursorWhenSelecting: true,
+	
+	allowDropFileTypes: ['.htm', '.html', '.htmls', '.htt', '.htx', 'shtml'],
+	
+	electricChars: true,		// Configures whether the editor should re-indent the current line
+								// when a character is typed that might change its proper indentation
+								//(only works if the mode supports indentation). Default is true.
+	
+	keyMap: 'sublime',			// uses Sublime Text keymapping
+								// down at the bottom of this script you can see the key commands
+								// please note that I added this script: ../keymap/sublime.js
+	
+	autoCloseBrackets: true,	// typed characters like ()[]{}''"" don't close automatically,
+								// please note that I added this script: ../addon/edit/closebrackets.js
+								// now type ( [ { ' or " in the editor
+	
+	autoCloseTags: true,		// tags in html/xml don't close automatically,
+								// please note that I added these scripts: ../addon/fold/xml-fold.js
+								//										   ../addon/edit/closetag.js
+								// now type <div> in the editor
+});
+console.log(htmlEditor);
+/**
+ *
+ * Sublime Text keyMap
+ *
+ * not all functions will be available,
+ * since some functions depend on other addon scripts
+ *
+ *
+ * key bindings:
+ *
+ * Alt-Left: 				"goSubwordLeft",
+ * Alt-Right: 				"goSubwordRight",
+ * Ctrl-Up: 				"scrollLineUp",
+ * Ctrl-Down: 				"scrollLineDown",
+ * Shift-Ctrl-L: 			"splitSelectionByLine",
+ * Shift-Tab: 				"indentLess",
+ * Esc: 					"singleSelectionTop",
+ * Ctrl-L: 					"selectLine",
+ * Shift-Ctrl-K: 			"deleteLine",
+ * Ctrl-Enter: 				"insertLineAfter",
+ * Shift-Ctrl-Enter: 		"insertLineBefore",
+ * Ctrl-D: 					"selectNextOccurrence",
+ * Alt-CtrlUp: 				"addCursorToPrevLine",
+ * Alt-CtrlDown: 			"addCursorToNextLine",
+ * Shift-Ctrl-Space:		"selectScope",
+ * Shift-Ctrl-M: 			"selectBetweenBrackets",
+ * Ctrl-M: 					"goToBracket",
+ * Shift-Ctrl-Up: 			"swapLineUp",
+ * Shift-Ctrl-Down: 		"swapLineDown",
+ * Ctrl-/: 					"toggleCommentIndented",
+ * Ctrl-J:		 			"joinLines",
+ * Shift-Ctrl-D: 			"duplicateLine",
+ * Ctrl-T: 					"transposeChars",
+ * F9: 						"sortLines",
+ * Ctrl-F9: 				"sortLinesInsensitive",
+ * F2: 						"nextBookmark",
+ * Shift-F2: 				"prevBookmark",
+ * Ctrl-F2: 				"toggleBookmark",
+ * Shift-Ctrl-F2: 			"clearBookmarks",
+ * Alt-F2: 					"selectBookmarks",
+ * Alt-Q: 					"wrapLines",
+ * Ctrl-K Ctrl-Backspace: 	"delLineLeft",
+ * Backspace: 				"smartBackspace",
+ * Ctrl-K Ctrl-K: 			"delLineRight",
+ * Ctrl-K Ctrl-U: 			"upcaseAtCursor",
+ * Ctrl-K Ctrl-L: 			"downcaseAtCursor",
+ * Ctrl-K Ctrl-Space: 		"setSublimeMark",
+ * Ctrl-K Ctrl-A: 			"selectToSublimeMark",
+ * Ctrl-K Ctrl-W: 			"deleteToSublimeMark",
+ * Ctrl-K Ctrl-X: 			"swapWithSublimeMark",
+ * Ctrl-K Ctrl-Y: 			"sublimeYank",
+ * Ctrl-K Ctrl-G: 			"clearBookmarks",
+ * Ctrl-K Ctrl-C: 			"showInCenter",
+ * Ctrl-Alt-Up: 			"selectLinesUpward",
+ * Ctrl-Alt-Down: 			"selectLinesDownward",
+ * Ctrl-F3: 				"findUnder",
+ * Shift-Ctrl-F3: 			"findUnderPrevious",
+ * Shift-Ctrl-[: 			"fold",
+ * Shift-Ctrl-]: 			"unfold",
+ * Ctrl-K Ctrl-J: 			"unfoldAll",
+ * Ctrl-K Ctrl-0: 			"unfoldAll",
+ * Ctrl-H: 					"replace",
+ *
+ */
+
